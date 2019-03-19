61d4d2256b78514ec85d342d835d2653994ae2fa
EduardoVillalobos <eduardo.vn03@gmail.com>
2019-03-19 01:48:22 -0600

    correcciones residencias

--- assets/js/residency/gallery.js
+++ assets/js/residency/gallery.js
@@ -5,30 +5,30 @@ function gallery1() {
 
     var slideIndex = 1;
     showDivs(slideIndex);
-        
+
     function plusDivs(n) {
         showDivs(slideIndex += n);
     }
-                    
-    document.querySelector('#button-left-1').addEventListener('click', function(n) {
+
+    document.querySelector('#button-left-1').addEventListener('click', function (n) {
         plusDivs(-1);
     });
-                    
-    document.querySelector('#button-right-1').addEventListener('click', function(n) {
+
+    document.querySelector('#button-right-1').addEventListener('click', function (n) {
         plusDivs(1);
     });
-                    
+
     function showDivs(n) {
         var i;
         var x = document.getElementsByClassName("mySlides1");
-        if (n > x.length) {slideIndex = 1}
-        if (n < 1) {slideIndex = x.length}
+        if (n > x.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = x.length }
         for (i = 0; i < x.length; i++) {
             x[i].style.display = "none";
         }
-        x[slideIndex-1].style.display = "block";
+        x[slideIndex - 1].style.display = "block";
     }
-                      
+
 }
 
 /* G A L L E R Y  T W O */
@@ -95,4 +95,235 @@ function gallery3() {
         x[slideIndex-1].style.display = "block";
     }
                       
+}
+
+/* G A L L E R Y  FOUR */
+gallery4();
+
+function gallery4() {
+
+    var slideIndex = 1;
+    showDivs(slideIndex);
+
+    function plusDivs(n) {
+        showDivs(slideIndex += n);
+    }
+
+    document.querySelector('#button-left-4').addEventListener('click', function (n) {
+        plusDivs(-1);
+    });
+
+    document.querySelector('#button-right-4').addEventListener('click', function (n) {
+        plusDivs(1);
+    });
+
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName("mySlides4");
+        if (n > x.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = x.length }
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        x[slideIndex - 1].style.display = "block";
+    }
+
+}
+
+/* G A L L E R Y  FIVE */
+gallery5();
+
+function gallery5() {
+
+    var slideIndex = 1;
+    showDivs(slideIndex);
+
+    function plusDivs(n) {
+        showDivs(slideIndex += n);
+    }
+
+    document.querySelector('#button-left-5').addEventListener('click', function (n) {
+        plusDivs(-1);
+    });
+
+    document.querySelector('#button-right-5').addEventListener('click', function (n) {
+        plusDivs(1);
+    });
+
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName("mySlides5");
+        if (n > x.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = x.length }
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        x[slideIndex - 1].style.display = "block";
+    }
+
+}
+
+/* G A L L E R Y  SIX */
+gallery6();
+
+function gallery6() {
+
+    var slideIndex = 1;
+    showDivs(slideIndex);
+
+    function plusDivs(n) {
+        showDivs(slideIndex += n);
+    }
+
+    document.querySelector('#button-left-6').addEventListener('click', function (n) {
+        plusDivs(-1);
+    });
+
+    document.querySelector('#button-right-6').addEventListener('click', function (n) {
+        plusDivs(1);
+    });
+
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName("mySlides6");
+        if (n > x.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = x.length }
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        x[slideIndex - 1].style.display = "block";
+    }
+
+}
+
+/* G A L L E R Y  SEVEN */
+gallery7();
+
+function gallery7() {
+
+    var slideIndex = 1;
+    showDivs(slideIndex);
+
+    function plusDivs(n) {
+        showDivs(slideIndex += n);
+    }
+
+    document.querySelector('#button-left-7').addEventListener('click', function (n) {
+        plusDivs(-1);
+    });
+
+    document.querySelector('#button-right-7').addEventListener('click', function (n) {
+        plusDivs(1);
+    });
+
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName("mySlides7");
+        if (n > x.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = x.length }
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        x[slideIndex - 1].style.display = "block";
+    }
+
+}
+
+/* G A L L E R Y  EIGHT */
+gallery8();
+
+function gallery8() {
+
+    var slideIndex = 1;
+    showDivs(slideIndex);
+
+    function plusDivs(n) {
+        showDivs(slideIndex += n);
+    }
+
+    document.querySelector('#button-left-8').addEventListener('click', function (n) {
+        plusDivs(-1);
+    });
+
+    document.querySelector('#button-right-8').addEventListener('click', function (n) {
+        plusDivs(1);
+    });
+
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName("mySlides8");
+        if (n > x.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = x.length }
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        x[slideIndex - 1].style.display = "block";
+    }
+
+}
+
+/* G A L L E R Y  NINE */
+gallery9();
+
+function gallery9() {
+
+    var slideIndex = 1;
+    showDivs(slideIndex);
+
+    function plusDivs(n) {
+        showDivs(slideIndex += n);
+    }
+
+    document.querySelector('#button-left-9').addEventListener('click', function (n) {
+        plusDivs(-1);
+    });
+
+    document.querySelector('#button-right-9').addEventListener('click', function (n) {
+        plusDivs(1);
+    });
+
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName("mySlides9");
+        if (n > x.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = x.length }
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        x[slideIndex - 1].style.display = "block";
+    }
+
+}
+
+/* G A L L E R Y  TEN */
+gallery10();
+
+function gallery10() {
+
+    var slideIndex = 1;
+    showDivs(slideIndex);
+
+    function plusDivs(n) {
+        showDivs(slideIndex += n);
+    }
+
+    document.querySelector('#button-left-10').addEventListener('click', function (n) {
+        plusDivs(-1);
+    });
+
+    document.querySelector('#button-right-10').addEventListener('click', function (n) {
+        plusDivs(1);
+    });
+
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName("mySlides10");
+        if (n > x.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = x.length }
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        x[slideIndex - 1].style.display = "block";
+    }
+
 }
